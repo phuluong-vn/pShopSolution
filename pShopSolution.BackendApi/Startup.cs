c0bd3263275b3ffe2c8823b4edfbf685cda9e908
tuan phu luong <phu@corsivalab.space>
2020-11-20 11:01:48 +0700

    Add Swagger for BackendAPI project

--- pShopSolution.BackendApi/Startup.cs
+++ pShopSolution.BackendApi/Startup.cs
@@ -1,17 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using pShopSolution.Application.Catalog.Products;
 using pShopSolution.Data.EF;
 using pShopSolution.Utilities.Constants;
+using System;
 
 namespace pShopSolution.BackendApi
 {
@@ -34,6 +31,16 @@ namespace pShopSolution.BackendApi
             services.AddTransient<IPublicProductService, PublicProductService>();
 
             services.AddControllersWithViews();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "ToDo API",
+                    Description = "A simple example ASP.NET Core Web API",
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -51,6 +58,13 @@ namespace pShopSolution.BackendApi
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            //add swagger
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Swagger PShopSolutionAPI");
+            });
 
             app.UseRouting();
 
