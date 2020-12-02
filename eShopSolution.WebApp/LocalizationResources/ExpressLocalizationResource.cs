1e431266d8ceb221e32cb7c9c9a4db61a06a32c6
tuan phu luong <phu@corsivalab.space>
2020-12-02 16:31:09 +0700

    multiple language

---
+++ eShopSolution.WebApp/LocalizationResources/ExpressLocalizationResource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pShopSolution.WebApp.LocalizationResources
+{
+    public class ExpressLocalizationResource
+    {
+    }
+}
--- eShopSolution.WebApp/Startup.cs
+++ eShopSolution.WebApp/Startup.cs
@@ -1,13 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
+using LazZiya.ExpressLocalization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using pShopSolution.WebApp.LocalizationResources;
 
 namespace eShopSolution.WebApp
 {
@@ -23,7 +27,24 @@ namespace eShopSolution.WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            var cultures = new CultureInfo[]
+            {
+                new CultureInfo("vi"),
+                new CultureInfo("en"),
+            };
+
+            services.AddControllersWithViews()
+                .AddExpressLocalization<ExpressLocalizationResource, ViewLocalizationResource>(ops =>
+                {
+                    ops.UseAllCultureProviders = false;
+                    ops.ResourcesPath = "LocalizationResources";
+                    ops.RequestLocalizationOptions = o =>
+                    {
+                        o.SupportedCultures = cultures;
+                        o.SupportedUICultures = cultures;
+                        o.DefaultRequestCulture = new RequestCulture("vi");
+                    };
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,12 +66,12 @@ namespace eShopSolution.WebApp
             app.UseRouting();
 
             app.UseAuthorization();
-
+            app.UseRequestLocalization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{culture=vi}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
